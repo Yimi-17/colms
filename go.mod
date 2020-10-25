cb528a453130db4ee21d351a292e5b5b3385d0c1
Yimi-17 <cguidoyimi@gmail.com>
2020-10-24 23:16:52 -0500

    Subir

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yimi-17/colms
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	f :="apple"
+	f ="ios"
+	var v string
+	v="wind"
+	fmt.Println("Hello, World"+ f + f,v)
+}
